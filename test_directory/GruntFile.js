84a4bcf2fb418780da7d75e2f0cd5593f5354458
Tokareva Tanya <tokareva-tatyanka@mail.ru>
2016-05-28 02:03:42 +0300

    swetu

---
+++ test_directory/GruntFile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    concat: {
+  options: {
+    separator: ';'
+  },
+  dist: {
+    src: ['js/*.js'],
+    dest: 'js/script.main.js'
+  }
+}
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+
+  grunt.registerTask('default', ['concat']);
+
+};
