279e4928f4cb8a748671c025f08654d6da5acf56
Tokareva Tanya <tokareva-tatyanka@mail.ru>
2016-05-31 00:55:24 +0300

    d

--- test_directory/GruntFile.js
+++ test_directory/GruntFile.js
@@ -1,20 +1,46 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+	  
     concat: {
   options: {
-    separator: ';'
+    separator: ' \r\n'
   },
+  
   dist: {
-    src: ['js/*.js'],
-    dest: 'js/script.main.js'
+    src: ['js/src/*.js'],
+    dest: 'js/dist/script.main.js'
+  },
+  
+  dist: {
+    src: ['css/src/*.css'],
+    dest: 'css/dist/main.css'
+  }
+},
+
+uglify:{
+	dist: {
+    src: ['js/dist/script.main.js'],
+    dest: 'js/dist/script.main.min.js'
+  } 
+},
+
+cssmin: {
+  options: {
+    separator: ''
+  },
+  
+  dist: {
+    src: ['css/src/*.css'],
+    dest: 'css/dist/main.min.css'
   }
 }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-
-  grunt.registerTask('default', ['concat']);
-
+grunt.registerTask('default', ['concat','uglify','cssmin']);
 };
+
